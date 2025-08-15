432f80ff0535de5e6836c24dab1ec9662980f20b
Alec Faustino <alecfaustino@outlook.com>
2025-08-15 15:17:19 -0600

    refactor spoon fetch code and fetch 10 random items

--- backend/routes/spoonacular-api.js
+++ backend/routes/spoonacular-api.js
@@ -1,23 +1,41 @@
 import express from "express";
-const router = express.Router();
-
 import axios from "axios";
+// API Documentation: https://spoonacular.com/food-api/docs
+const RECIPES_BASE_URL = "https://api.spoonacular.com/recipes";
 
+const router = express.Router();
 // fetch a single random recipe
 router.get("/random", async (req, res) => {
   const apiKey = process.env.SPOON_API_KEY;
   try {
-    const response = await axios.get(
-      `https://api.spoonacular.com/recipes/random`,
-      {
-        params: {
-          apiKey,
-        },
-      }
-    );
+    const response = await axios.get(`${RECIPES_BASE_URL}/random`, {
+      params: {
+        apiKey,
+      },
+    });
     const data = response.data;
 
     res.status(200).json({ message: "Random recipe fetched: ", data });
+  } catch (error) {
+    console.error("Error fetching random recipe ", error);
+    res.status(500).json({ message: "Internal Server Error", error });
+  }
+});
+
+// fetch 10 random recipes
+
+router.get("/random10", async (req, res) => {
+  const apiKey = process.env.SPOON_API_KEY;
+  try {
+    const response = await axios.get(`${RECIPES_BASE_URL}/random`, {
+      params: {
+        apiKey,
+        number: 10,
+      },
+    });
+    const data = response.data;
+
+    res.status(200).json({ message: "10 recipes fetched: ", data });
   } catch (error) {
     console.error("Error fetching random recipe ", error);
     res.status(500).json({ message: "Internal Server Error", error });
